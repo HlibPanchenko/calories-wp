53c3446906dc41809742647387c7fcae1fe58511
HlibPanchenko <glebpanchenk7@gmail.com>
2024-01-07 14:01:28 +0200

    404.php, more acf fields for taxonomies block

--- 404.php
+++ 404.php
@@ -13,45 +13,11 @@ get_header();
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'calories_first' ); ?></h1>
-			</header><!-- .page-header -->
+            <p> Упс, произошла ошибка. </p>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'calories_first' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'calories_first' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$calories_first_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'calories_first' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$calories_first_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
+            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+			Вернуться на главную страницу.
+            </a>
 		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
--- template-parts/blocks/popular-taxonomies-block.php
+++ template-parts/blocks/popular-taxonomies-block.php
@@ -1,30 +1,74 @@
 <?php
+
 use src\UtilsClass;
+
+$uniqid = uniqid('taxonomies-block-');
+
+
 // Получаем ID страницы "Main page" чтобы получить ее custom fields
 //$main_page_id = get_option('page_on_front');
 
 // Получаем кастомные поля
 $popular_categories = get_field('popular_categories'); // group
 //$popular_categories = get_field('popular_categories', $main_page_id); // group
+$popular_categories_background = $popular_categories["popular_categories_background"];
 $popular_categories_title = $popular_categories["popular_categories_title"]; // text
+$popular_categories_title_color = $popular_categories["popular_categories_title_color"]; // title color
+$popular_categories_text_color = $popular_categories["popular_categories_text_color"];
 $popular_categories_taxonomies = $popular_categories["popular_categories_taxonomies"]; // checkbox
 $popular_categories_link = $popular_categories["popular_categories_link"];
+
+
 // Определение белого списка таксономий
 $whitelist_taxonomies = array('dish', 'ingredients', 'cuisine', 'holiday', 'cooking-method', 'part_of_the_day');
 
 ?>
 
-<section class="recipe-layout popular-taxonomies">
+<style>
+    <?php
+
+    echo <<<EOT
+    #$uniqid {
+         background: $popular_categories_background;
+         }
+    
+    #$uniqid .header-layout_linktext {
+        color: $popular_categories_title_color;
+    }
+    
+      #$uniqid .header-layout_linktext:hover {
+            border-bottom: 2px solid $popular_categories_title_color; 
+      }
+      
+     #$uniqid .header-layout_svg-wrapper {
+     path {
+        stroke:$popular_categories_title_color;
+           }
+     }
+     
+      #$uniqid .card-layout_title {
+        color: $popular_categories_text_color;
+    }
+     
+    EOT;
+    ?>
+</style>
+
+<section class="recipe-layout popular-taxonomies" id="<?php echo $uniqid; ?>">
     <div class="recipe-layout_container">
         <header class="recipe-layout_header header-layout">
             <div class=" header-layout_wrapper">
-                <a href="<?php echo esc_url(home_url('/' . $popular_categories_link )); ?>" class=" header-layout_link shaked-el" >
-<!--                <a href="--><?php //echo esc_url(get_permalink(get_page_by_path('recipes'))); ?><!--" class=" header-layout_link shaked-el" >-->
+                <a href="<?php echo esc_url(home_url('/' . $popular_categories_link)); ?>"
+                   class=" header-layout_link shaked-el">
+                    <!--                <a href="-->
+                    <?php //echo esc_url(get_permalink(get_page_by_path('recipes'))); ?><!--" class=" header-layout_link shaked-el" >-->
 
-                    <span class="header-layout_linktext _anim-items"><?php echo  $popular_categories_title?></span>
+                    <span class="header-layout_linktext _anim-items"><?php echo $popular_categories_title ?></span>
                     <span class="header-layout_svg-wrapper">
-                        <svg width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M4 12H20M20 12L16 8M20 12L16 16" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <svg width="36px" height="36px" viewBox="0 0 24 24" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+<path d="M4 12H20M20 12L16 8M20 12L16 16" stroke="#000000" stroke-width="2" stroke-linecap="round"
+      stroke-linejoin="round"/>
 </svg>
                     </span>
                 </a>
@@ -59,9 +103,11 @@ $whitelist_taxonomies = array('dish', 'ingredients', 'cuisine', 'holiday', 'cook
                                     <a href="<?php echo get_term_link($term_id); ?>" class="card-layout_link">
                                         <div class="card-layout_header">
                                             <?php if ($taxonomy_image) : ?>
-                                                <img src="<?php echo esc_url($taxonomy_image); ?>" alt="Картинка для таксономии" class="card-layout_img">
+                                                <img src="<?php echo esc_url($taxonomy_image); ?>"
+                                                     alt="Картинка для таксономии" class="card-layout_img">
                                             <?php else : ?>
-                                                <img src="<?php echo get_template_directory_uri(); ?>/dist/images/default_taxonomy.png" alt="Default Image" class="card-layout_img">
+                                                <img src="<?php echo get_template_directory_uri(); ?>/dist/images/default_taxonomy.png"
+                                                     alt="Default Image" class="card-layout_img">
                                             <?php endif; ?>
                                         </div>
                                         <div class="card-layout_info">
