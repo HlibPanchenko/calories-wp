deffff25b17a8c16f48f800a7deedea6747c2234
HlibPanchenko <glebpanchenk7@gmail.com>
2024-01-22 15:28:19 +0200

    do not show search in header, show icon of account, on phones show only svg

--- header.php
+++ header.php
@@ -100,9 +100,13 @@ Header
 
                                     if ($current_user instanceof WP_User) :
                                         $btnText = $current_user->user_login ? esc_html($current_user->user_login) : 'Личный кабинет';
-                                        echo $btnText;
                                     endif;
                                     ?>
+                                    <div class="main-header_signin_text">
+                                        <?php
+                                        echo $btnText;
+                                        ?>
+                                    </div>
                                 </div>
                             </a>
                         <?php endif; ?>
@@ -117,7 +121,7 @@ Header
                                         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"></path>
                                     </g>
                                 </svg>
-                                <div>Вход</div>
+                                <div class="main-header_signin_text">Вход</div>
                             </a>
                         <?php endif; ?>
                     </div>
