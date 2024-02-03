c190c6a0c2b2b7fd137e4c4ebd4fe58fa65e0fba
HlibPanchenko <glebpanchenk7@gmail.com>
2024-02-03 23:06:07 +0200

    do not log out users, unfortunately

--- src/UserAuthHandler.php
+++ src/UserAuthHandler.php
@@ -20,8 +20,9 @@ class UserAuthHandler
 
             if ($has_activated == false) {
                 // Если не админ, разлогиниваем
-                if ($post_id !== 'user_1') {
-                    wp_logout($post_id);
+                /*Разлогинивает не пользователей у которых has activated false, а админа который внес изменения*/
+//                if ($post_id !== 'user_1') {
+//                    wp_logout($post_id);
                 }
 
 //                if ($user_id !== 1) {  // 1 - это ID админа
@@ -30,22 +31,3 @@ class UserAuthHandler
             }
         }
     }
-
-//    public static function check_has_activated_change($post_id) {
-//        // Проверяем, что сохраняемый пост является пользовательским профилем
-//        if (strpos($post_id, 'user_') === 0) {
-//            // Приходит например 'user_123'. Отрезаем "user_" и оставляем только ID
-////            $post_id = (int) substr($post_id, strlen('user_'));
-//
-//            $has_activated = get_field('has_activated', $post_id);
-//            $_SESSION['changed_post_id'] =  $has_activated;
-//
-//            if ($has_activated !== 1) {
-//                // Получаем ID пользователя
-//                $user_id = (int)str_replace('user_', '', $post_id);
-//                // Деавторизация пользователя
-//                wp_logout($user_id);
-//            }
-//        }
-//    }
-}
