1010f4b2dcb35f7a0b54a94027314d77fce1770b
HlibPanchenko <glebpanchenk7@gmail.com>
2024-02-03 22:45:44 +0200

    logoout user if false try 2

--- front-page.php
+++ front-page.php
@@ -6,6 +6,25 @@ get_header();
 ?>
 
     <main id="primary" class="main-wrapper">
+<!--        --><?php
+//        if (isset($_SESSION['changed_post_id'])) {
+//            echo '<div class="success-message">
+//        <div class="success-message_wrapper">
+//            <div class="success-message_svg">
+//                <svg width="34px" height="34px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#ffffff" d="M512 64a448 448 0 1 1 0 896 448 448 0 0 1 0-896zm-55.808 536.384-99.52-99.584a38.4 38.4 0 1 0-54.336 54.336l126.72 126.72a38.272 38.272 0 0 0 54.336 0l262.4-262.464a38.4 38.4 0 1 0-54.272-54.336L456.192 600.384z"></path></g></svg>
+//
+//            </div>
+//            <div class="success-message_content">
+//                <div class="success-message_text">' . esc_html($_SESSION['changed_post_id']) . '</div>
+//            </div>
+//            <div class="success-message_close">&times;</div>
+//        </div>
+//    </div>';
+//            // Очистка сообщения после отображения
+//            unset($_SESSION['changed_post_id']);
+//        }
+//
+//        ?>
         <?php
         if (isset($_SESSION['recipe_added'])) {
             echo '<div class="success-message">
--- src/UserAuthHandler.php
+++ src/UserAuthHandler.php
@@ -12,18 +12,40 @@ class UserAuthHandler
     public static function check_has_activated_change($post_id) {
         // Проверяем, что сохраняемый пост является пользовательским профилем
         if (strpos($post_id, 'user_') === 0) {
-            // Приходит например 'user_123'. Отрезаем "user_" и оставляем только ID
-//            $post_id = (int) substr($post_id, strlen('user_'));
+            // Отрезаем "user_" и оставляем только ID
+            $user_id = (int) substr($post_id, strlen('user_'));
 
-            $has_activated = get_field('has_activated', $post_id);
-//            $_SESSION['changed_post_id'] = 'Прошлое значение: ' . $has_activated;
+            $has_activated = get_field('has_activated', 'user_' . $user_id);
+//            $_SESSION['changed_post_id'] =  $post_id;
 
-            if ($has_activated !== 1) {
-                // Получаем ID пользователя
-                $user_id = (int)str_replace('user_', '', $post_id);
-                // Деавторизация пользователя
-                wp_logout($user_id);
+            if ($has_activated == false) {
+                // Если не админ, разлогиниваем
+                if ($post_id !== 'user_1') {
+                    wp_logout($post_id);
+                }
+
+//                if ($user_id !== 1) {  // 1 - это ID админа
+//                    wp_logout($user_id);
+//                }
             }
         }
     }
+
+//    public static function check_has_activated_change($post_id) {
+//        // Проверяем, что сохраняемый пост является пользовательским профилем
+//        if (strpos($post_id, 'user_') === 0) {
+//            // Приходит например 'user_123'. Отрезаем "user_" и оставляем только ID
+////            $post_id = (int) substr($post_id, strlen('user_'));
+//
+//            $has_activated = get_field('has_activated', $post_id);
+//            $_SESSION['changed_post_id'] =  $has_activated;
+//
+//            if ($has_activated !== 1) {
+//                // Получаем ID пользователя
+//                $user_id = (int)str_replace('user_', '', $post_id);
+//                // Деавторизация пользователя
+//                wp_logout($user_id);
+//            }
+//        }
+//    }
 }
